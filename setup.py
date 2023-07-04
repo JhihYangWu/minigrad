a535751eed5412e3f84f55ca7bc9c8ce5450c882
JhihYang Wu <jhihyang_wu@outlook.com>
2023-07-04 12:53:55 -0700

    Add setup.py.

---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(name="minigrad",
+                 version="0.1",
+                 description="Rip-off of tinygrad",
+                 url="#",
+                 author="JhihYang Wu",
+                 install_requires=["numpy"],
+                 author_email="",
+                 packages=setuptools.find_packages(),
+                 zip_safe=False)
+
