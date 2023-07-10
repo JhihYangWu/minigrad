d78b4e947651411e0e0d1d81a3d8d87bcd53c41e
JhihYang Wu <jhihyang_wu@outlook.com>
2023-07-10 12:55:56 -0700

    Use batchnorm1d to speed up training in test cases.

--- test/test_mnist.py
+++ test/test_mnist.py
@@ -7,8 +7,8 @@ class TestMNIST(unittest.TestCase):
     def test_mnist(self):
         x_train, y_train, x_test, y_test = get_mnist()
         model = FullyConnectedModel()
-        optimizer = optim.SGD(model.params())
-        optim.train(x_train, y_train, model, optimizer, "CategoricalCrossentropy", 1000, 256)
+        optimizer = optim.SGD(model.params(), lr=0.01)
+        optim.train(x_train, y_train, model, optimizer, "CategoricalCrossentropy", 200, 256)
         test_loss, test_acc = optim.evaluate(x_test, y_test, model, "CategoricalCrossentropy", 256)
         print("Test Loss: %.2f | Test Accuracy: %.2f" % (test_loss, test_acc))
         assert test_acc >= 0.9
@@ -16,8 +16,8 @@ class TestMNIST(unittest.TestCase):
     def test_mnist_momentum(self):
         x_train, y_train, x_test, y_test = get_mnist()
         model = FullyConnectedModel()
-        optimizer = optim.Momentum(model.params(), lr=0.0001)
-        optim.train(x_train, y_train, model, optimizer, "CategoricalCrossentropy", 1000, 256)
+        optimizer = optim.Momentum(model.params(), lr=0.01)
+        optim.train(x_train, y_train, model, optimizer, "CategoricalCrossentropy", 200, 256)
         test_loss, test_acc = optim.evaluate(x_test, y_test, model, "CategoricalCrossentropy", 256)
         print("Test Loss: %.2f | Test Accuracy: %.2f" % (test_loss, test_acc))
         assert test_acc >= 0.9
@@ -25,8 +25,8 @@ class TestMNIST(unittest.TestCase):
     def test_mnist_rmsprop(self):
         x_train, y_train, x_test, y_test = get_mnist()
         model = FullyConnectedModel()
-        optimizer = optim.RMSprop(model.params(), lr=0.0001)
-        optim.train(x_train, y_train, model, optimizer, "CategoricalCrossentropy", 1000, 256)
+        optimizer = optim.RMSprop(model.params(), lr=0.01)
+        optim.train(x_train, y_train, model, optimizer, "CategoricalCrossentropy", 200, 256)
         test_loss, test_acc = optim.evaluate(x_test, y_test, model, "CategoricalCrossentropy", 256)
         print("Test Loss: %.2f | Test Accuracy: %.2f" % (test_loss, test_acc))
         assert test_acc >= 0.9
@@ -34,8 +34,8 @@ class TestMNIST(unittest.TestCase):
     def test_mnist_adam_lr_decay(self):
         x_train, y_train, x_test, y_test = get_mnist()
         model = FullyConnectedModel()
-        optimizer = optim.Adam(model.params(), lr=0.001, lr_decay=0.5, steps_per_epoch=x_train.shape[0]/256)
-        optim.train(x_train, y_train, model, optimizer, "CategoricalCrossentropy", 1000, 256)
+        optimizer = optim.Adam(model.params(), lr=0.1, lr_decay=0.5, steps_per_epoch=x_train.shape[0]/256)
+        optim.train(x_train, y_train, model, optimizer, "CategoricalCrossentropy", 200, 256)
         test_loss, test_acc = optim.evaluate(x_test, y_test, model, "CategoricalCrossentropy", 256)
         print("Test Loss: %.2f | Test Accuracy: %.2f" % (test_loss, test_acc))
         assert test_acc >= 0.9
@@ -54,12 +54,21 @@ class FullyConnectedModel:
         self.l1 = Tensor.rand_init(784, 128)
         self.l2 = Tensor.rand_init(128, 64)
         self.l3 = Tensor.rand_init(64, 10)
+        self.g1 = Tensor(np.random.uniform(size=128))
+        self.b1 = Tensor.rand_init(128)
+        self.g2 = Tensor(np.random.uniform(size=64))
+        self.b2 = Tensor.rand_init(64)
 
     def params(self):
-        return [self.l1, self.l2, self.l3]
+        return [self.l1, self.l2, self.l3, self.g1, self.b1, self.g2, self.b2]
 
     def forward(self, x):
-        return x.matmul(self.l1).relu().matmul(self.l2).relu().matmul(self.l3).softmax(dim=1)
+        loss = x.matmul(self.l1)
+        loss = loss.batchnorm1d(self.g1, self.b1)
+        loss = loss.relu().matmul(self.l2)
+        loss = loss.batchnorm1d(self.g2, self.b2)
+        loss = loss.relu().matmul(self.l3).softmax(dim=1)
+        return loss
 
 class ConvModel:
     def __init__(self):
