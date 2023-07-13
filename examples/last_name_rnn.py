f91ef15fe90c7eba75c4b076892bc2ac0daebf83
JhihYang Wu <jhihyang_wu@outlook.com>
2023-07-13 11:54:36 -0700

    Add load_data().

--- examples/last_name_rnn.py
+++ examples/last_name_rnn.py
@@ -6,17 +6,27 @@
 
 import unicodedata
 import string
+import os
 
 ALL_LETTERS = string.ascii_letters + ".,;'"
 
 def main():
-    pass
+    names = load_data()
 
 def unicode_to_ascii(s):
     return "".join(
         c for c in unicodedata.normalize("NFD", s)
         if unicodedata.category(c) != "Mn" and c in ALL_LETTERS
     )
+
+def load_data():
+    names = {}
+    for file in os.listdir("names/"):
+        if file.endswith(".txt"):
+            language = file[:-4]
+            lines = open("names/" + file, encoding="utf-8").read().strip().split("\n")
+            names[language] = [unicode_to_ascii(n) for n in lines]
+    return names
 
 if __name__ == "__main__":
     main()
