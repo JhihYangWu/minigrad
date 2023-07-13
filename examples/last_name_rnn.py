1019132e7e74ff72fffc30b4e45a947507dbf4a0
JhihYang Wu <jhihyang_wu@outlook.com>
2023-07-13 11:33:11 -0700

    Setup RNN example.

---
+++ examples/last_name_rnn.py
@@ -0,0 +1,11 @@
+# Showcase how minigrad can be used to recognize which language a last name is in.
+
+# Download training data from https://download.pytorch.org/tutorial/data.zip
+# and put names folder from data.zip next to this script.
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
+
