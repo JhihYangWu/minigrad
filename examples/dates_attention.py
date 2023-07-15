f930a089e70c00e17f2868e4bb7a906940135881
JhihYang Wu <jhihyang_wu@outlook.com>
2023-07-15 11:18:16 -0700

    Setup attention example.

---
+++ examples/dates_attention.py
@@ -0,0 +1,14 @@
+# Showcase how minigrad can be used to create attention models.
+
+# Task: train an attention model to translate dates into standard format.
+# Example input: saturday december 10 1994
+# Expected output: 1994-12-10
+# Example input: feb 5 2002
+# Expected output: 2002-02-05
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
+
