dfd52994c314341d21ae83c1fa852501eb67421e
JhihYang Wu <jhihyang_wu@outlook.com>
2023-07-15 17:41:48 -0700

    Setup script.

--- examples/dates_attention_test.py
+++ examples/dates_attention_test.py
@@ -1,13 +1,22 @@
 # Test script for dates_attention_train.py
 
 import pickle
-from dates_attention_train import ATT_NN, TOP_LSTM, BOT_LSTM
+from dates_attention_train import ATT_NN, TOP_LSTM, BOT_LSTM, random_date
+from faker import Faker
+import time
 
 def main():
     rightward = pickle.load(open("dates_attention_trained/rightward.pickle", "rb"))
     leftward = pickle.load(open("dates_attention_trained/leftward.pickle", "rb"))
     small_nn = pickle.load(open("dates_attention_trained/small_nn.pickle", "rb"))
     main_lstm = pickle.load(open("dates_attention_trained/main_lstm.pickle", "rb"))
+    faker = Faker()
+    while True:
+        human_str, expected_str = random_date(faker)
+        print("Input:", human_str)
+        print("Expected Output:", expected_str)
+        print()
+        time.sleep(1)
 
 if __name__ == "__main__":
     main()
